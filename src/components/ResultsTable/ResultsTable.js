aa756597ee422f3e497b8b8fe32b5412b20ba221
bexalho <aljesa.hoxha@bexeo.com>
2023-06-29 16:08:30 +0100

    added CSS modules

--- src/components/ResultsTable/ResultsTable.js
+++ src/components/ResultsTable/ResultsTable.js
@@ -1,3 +1,5 @@
+import classes from "./ResultsTable.module.css";
+
 const formatter = new Intl.NumberFormat("en-EU", {
   style: "currency",
   currency: "EUR",
@@ -7,7 +9,7 @@ const formatter = new Intl.NumberFormat("en-EU", {
 
 const ResultsTable = (props) => {
   return (
-    <table className="result">
+    <table className={classes.result}>
       <thead>
         <tr>
           <th>Year</th>
